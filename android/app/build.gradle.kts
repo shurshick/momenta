ea7afdba9b36f43c41ab6d7edab3094a3272a568
Momenta Admin <admin@momenta.app>
2026-06-25 22:41:45 +0300

    fix(android): upgrade hilt-work to 1.2.0 with proper KSP support
    - Reverted kapt back to KSP for Hilt
    - Upgraded androidx.hilt:hilt-work and hilt-compiler from 1.1.0 to 1.2.0
    - 1.2.0 has native KSP support, fixing NonExistentClass resolution
    - Removed kapt plugin (no longer needed)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("com.google.dagger.hilt.android")
     id("com.google.devtools.ksp")
-    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -111,7 +110,7 @@ dependencies {
 
     // Hilt
     implementation("com.google.dagger:hilt-android:2.50")
-    kapt("com.google.dagger:hilt-compiler:2.50")
+    ksp("com.google.dagger:hilt-compiler:2.50")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     // Room
@@ -140,8 +139,8 @@ dependencies {
 
     // WorkManager
     implementation("androidx.work:work-runtime-ktx:2.9.0")
-    implementation("androidx.hilt:hilt-work:1.1.0")
-    kapt("androidx.hilt:hilt-compiler:1.1.0")
+    implementation("androidx.hilt:hilt-work:1.2.0")
+    ksp("androidx.hilt:hilt-compiler:1.2.0")
 
     // Accompanist
     implementation("com.google.accompanist:accompanist-permissions:0.34.0")
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -3,5 +3,4 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.9.20" apply false
 }
